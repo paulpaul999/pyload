585cb612ec4280778099059f39736245e1b8174d
paulpaul999 <82762352+paulpaul999@users.noreply.github.com>
2023-10-24 22:31:15 +0200

    BadoinkVR account plugin

---
+++ src/pyload/plugins/accounts/BadoinkVR.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# import re
+# import time
+from datetime import timedelta
+import json
+
+import pycurl
+# from pyload.core.network.http.exceptions import BadHeader
+
+from ..base.account import BaseAccount
+
+
+class BadoinkVR(BaseAccount):
+    __name__ = "BadoinkVR"
+    __type__ = "account"
+    __version__ = "0.1"
+    __status__ = "testing"
+
+    __description__ = """BadoinkVR account plugin"""
+    __license__ = "GPLv3"
+    __authors__ = [
+        ("paulpaul999", "no-mail@github.com")
+    ]
+
+    LOGIN_TIMEOUT = timedelta(days=7).total_seconds()
+
+    def grab_info(self, user, password, data):
+        premium = False
+        validuntil = None
+        trafficleft = None
+
+        token = data.get('token')
+
+        self.req.http.c.setopt(pycurl.USERAGENT, "HereSphere")
+        self.req.http.c.setopt(
+            pycurl.HTTPHEADER, [f"auth-token: {token}"]
+        )
+        response_ = self.load(
+            "https://badoinkvr.com/heresphere",
+            post=True,
+            cookies=False,
+            decode=False
+        )
+        response = json.loads(response_)
+
+        premium = response.get('access') == 1
+
+        return {
+            "premium": premium,
+            "validuntil": validuntil,
+            "trafficleft": trafficleft,
+        }
+
+    def signin(self, user, password, data):
+        self.req.http.c.setopt(pycurl.USERAGENT, "HereSphere")
+        post_payload = json.dumps( {"username": user, "password": password} )
+        response_ = self.load(
+            "https://badoinkvr.com/heresphere/auth",
+            post=post_payload,
+            cookies=False,
+            decode=False
+        )
+        response = json.loads(response_)
+
+        if not response['access'] == 1:
+            self.fail_login()
+
+        data['token'] = response['auth-token']
